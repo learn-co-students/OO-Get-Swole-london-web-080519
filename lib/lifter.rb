24229fb384c124762a8999834aa76bea7409afbb
github username <github email address>
2019-08-12 12:50:44 +0100

    Done

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,55 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
-    @lift_total = lift_total
+    @lift_total = lift_total.to_f
+    @@all << self 
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  # Get a list of all the memberships that a specific lifter has
+
+  def lifter_memberships 
+    Membership.all.select {|membership| membership.lifter == self}
+  end 
+
+  def lifter_gyms
+    self.lifter_memberships.map {|membership| membership.gym}
+  end 
+
+  def self.avg_lift_total 
+    lifts = self.all.map {|lifter| lifter.lift_total}
+    lifts.sum/lifts.count
+  end 
+
+  def total_mem_cost 
+    self.lifter_memberships.map {|membership| membership.cost}.sum 
+  end 
+
+
+# Given a gym and a membership cost, sign a specific lifter up for a new gym
+
+  def new_gym_signup(gym, cost)
+    Membership.new(self, gym, cost)
+  end 
+
 end
+
+# Get a list of all lifters XXXX
+
+# Get a list of all the memberships that a specific lifter has XXX
+
+# Get a list of all the gyms that a specific lifter has memberships to XXX
+
+# Get the average lift total of all lifters XXX
+
+# Get the total cost of a specific lifter's gym memberships XXX
+
+# Given a gym and a membership cost, sign a specific lifter up for a new gym XXX
+
--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,20 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+bob = Lifter.new("Bob", 200)
+joe = Lifter.new("Joe", 300)
+heather = Lifter.new("Heather", 400)
+
+soho_gym = Gym.new("Soho")
+shoreditch_gym = Gym.new("Shoreditch")
+brixton_gym = Gym.new("Brixton")
+
+membership1 = Membership.new(bob, soho_gym, 100)
+membership2 = Membership.new(joe, shoreditch_gym, 120) 
+membership3 = Membership.new(heather, brixton_gym, 90)
+membership4 = Membership.new(bob, shoreditch_gym, 120)
+membership5 = Membership.new(joe, brixton_gym, 95)
+
 
 binding.pry
 

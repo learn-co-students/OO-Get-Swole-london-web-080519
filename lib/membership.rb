50488c8c1bf1738089ef9a193c365d9dcc1f6c47
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-13 15:14:48 +0100

    Initial commit.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,18 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :plan_name, :gym, :lifter
+  @@all = []
 
-  def initialize(cost)
+  def initialize(cost:, plan_name:, gym:, lifter:)
     @cost = cost
+    @plan_name
+    @gym = gym
+    @lifter = lifter
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  
 end

6dfe356cc8aafe3037a00830fea2dc6f9720c141
github username <github email address>
2019-08-14 06:53:12 +0100

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,30 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|member| member.gym == self}
+  end
+
+  def lifters
+    memberships.map{|member| member.lifter}
+  end
+
+  def lifter_names
+    memberships.map{|member| member.lifter.name}
+  end
+
+  def lift_total
+    memberships.sum{|member| member.lifter.lift_total}
+  end
+
 end
